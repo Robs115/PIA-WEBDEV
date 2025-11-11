4cda2bd70f735b2b9abd3df7ea470c638cb621dd
NELSONRDZ20 <nelsonpuntordzdel@gmail.com>
2025-11-11 16:35:31 -0600

    puse upper al guardar models y quite las opciones de estatus al crear

--- veterinaria/app/views.py
+++ veterinaria/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cita_Veterinaria, Servicio
 from django.contrib.auth.decorators import login_required, permission_required
+from django.utils import timezone
+from django.contrib import messages
 
 def index(request):
     citas = Cita_Veterinaria.objects.all()
@@ -19,14 +21,14 @@ def listar_Cita_Veterinaria(request):
 @login_required
 def crear_Cita_Veterinaria(request):
     if request.method == 'POST':
-        nombre_dueño = request.POST['nombre_dueño']
-        nombre_mascota = request.POST['nombre_mascota']
-        especie = request.POST['especie']
+        nombre_dueño = request.POST['nombre_dueño'].upper()
+        nombre_mascota = request.POST['nombre_mascota'].upper()
+        especie = request.POST['especie'].upper()
         fecha_cita = request.POST['fecha_cita']
         hora_cita = request.POST['hora_cita']
         
-        estatus_cita = request.POST['estatus_cita']
-        descripcion = request.POST['descripcion']
+        estatus_cita = request.POST['estatus_cita'].upper()
+        descripcion = request.POST['descripcion'].upper()
         servicio_id = request.POST['servicio']
         servicio = Servicio.objects.get(id=servicio_id)
         Cita_Veterinaria.objects.create(nombre_dueño=nombre_dueño, nombre_mascota=nombre_mascota, especie=especie, fecha_cita=fecha_cita, hora_cita=hora_cita, servicio=servicio, estatus_cita=estatus_cita, descripcion=descripcion)
@@ -37,8 +39,9 @@ def crear_Cita_Veterinaria(request):
     contexto = {
         'servicio': listar_servicio
     }
-    
+
     return render (request, 'crear.html', contexto)
+
 
 
 @login_required
@@ -51,14 +54,14 @@ def editar_Cita_Veterinaria(request, id):
         if solo_estatus:
             cita_veterinaria.estatus_cita = request.POST['estatus_cita']
         else:
-            cita_veterinaria.nombre_dueño = request.POST['nombre_dueño']
-            cita_veterinaria.nombre_mascota = request.POST['nombre_mascota']
-            cita_veterinaria.especie = request.POST['especie']
+            cita_veterinaria.nombre_dueño = request.POST['nombre_dueño'].upper()
+            cita_veterinaria.nombre_mascota = request.POST['nombre_mascota'].upper()
+            cita_veterinaria.especie = request.POST['especie'].upper()
             cita_veterinaria.fecha_cita = request.POST['fecha_cita']
             cita_veterinaria.hora_cita = request.POST['hora_cita']
             cita_veterinaria.servicio = Servicio.objects.get(id=request.POST['servicio'])
-            cita_veterinaria.estatus_cita = request.POST['estatus_cita']
-            cita_veterinaria.descripcion = request.POST['descripcion']
+            cita_veterinaria.estatus_cita = request.POST['estatus_cita'].upper()
+            cita_veterinaria.descripcion = request.POST['descripcion'].upper()
 
         cita_veterinaria.save()
         return redirect('listar')
@@ -82,8 +85,8 @@ def eliminar_Cita_Veterinaria(request, id):
 @permission_required('app.delete_citaveterinaria', raise_exception=True)
 def crear_servicio(request):
     if request.method == 'POST':
-        nombre = request.POST['nombre']
-        descripcion = request.POST['descripcion']
+        nombre = request.POST['nombre'].upper()
+        descripcion = request.POST['descripcion'].upper()
         precio = request.POST['precio']
 
         Servicio.objects.create(
